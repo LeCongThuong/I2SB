fix removing masking

i2sb/diffusion.py
@@ -61,8 +61,8 @@ class Diffusion():
         xt = mu_x0 * x0 + mu_x1 * x1
         if not ot_ode:
             xt = xt + std_sb * torch.randn_like(xt)
-        if mask is not None:
-            xt = torch.where(mask, xt, torch.ones_like(xt))
+        # if mask is not None:
+        #     xt = torch.where(mask, xt, torch.ones_like(xt))
         return xt.detach()
 
     def p_posterior(self, nprev, n, x_n, x0, ot_ode=False, mask=None):
@@ -78,8 +78,8 @@ class Diffusion():
         xt_prev = mu_x0 * x0 + mu_xn * x_n
         if not ot_ode and nprev > 0:
             xt_prev = xt_prev + var.sqrt() * torch.randn_like(xt_prev)
-        if mask is not None:
-            xt_prev = torch.where(mask, xt_prev, torch.ones_like(xt_prev))
+        # if mask is not None:
+        #     xt_prev = torch.where(mask, xt_prev, torch.ones_like(xt_prev))
         return xt_prev
 
     def ddpm_sampling(self, steps, pred_x0_fn, x1, mask=None, ot_ode=False, log_steps=None, verbose=True):

i2sb/runner.py
@@ -106,7 +106,7 @@ class Runner(object):
         """ Eq 12 """
         std_fwd = self.diffusion.get_std_fwd(step, xdim=x0.shape[1:])
         label = (xt - x0) / std_fwd
-        label = torch.where(mask, label, torch.ones_like(label))
+        # label = torch.where(mask, label, torch.ones_like(label))
         return label.detach()
 
     def compute_pred_x0(self, step, xt, net_out, clip_denoise=False, mask=None):
@@ -192,7 +192,7 @@ class Runner(object):
                 aux_loss = self.aux_loss(img_reconst, x0)
                 noise_denoising_loss = F.mse_loss(noise_pred, label)
                 loss = noise_denoising_loss + aux_loss
-                
+
                 loss.backward()
                 batch_loss = batch_loss + loss
             loss = batch_loss / n_inner_loop